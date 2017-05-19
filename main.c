4adfedff4d5054751e223c42895404315d01bf30
GUILHERME-LAPTO\guilh <guilhermeditavares@gmail.com>
2017-05-19 19:20:33 +0100

    cenas ficheiros

--- main.c
+++ main.c
@@ -6,7 +6,9 @@ int main() {
     FILE *f_exames;
     FILE *f_alunos;
     FILE *f_disciplinas;
-    //f_exames = fopen("Ficheiros/Exames.txt", );
+    f_exames = fopen("Ficheiros/Exames.txt","a");
+    f_alunos = fopen("Ficheiros/Alunos.txt","a");
+    f_disciplinas = fopen("Ficheiros/Disciplinas.txt","a");
     menuPrincipal();
     return 0;
 }
